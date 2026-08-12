e944c63a1408f0bcd8838d50355f027a9da93f2c
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-08-12 16:02:41 +0530

    feat: add consol outputs in js

---
+++ 01-fundamentals/02-console/script.js
@@ -0,0 +1,12 @@
+// Basic console output
+console.log("Hello from JavaScript");
+
+// Display a warning
+console.warn("This is a warning");
+
+// Display an error
+console.error("This is an error");
+
+// Display multiple values
+console.log("Name:", "Tejas");
+console.log("Age:", 25);
