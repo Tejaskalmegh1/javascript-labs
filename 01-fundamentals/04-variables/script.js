19dad32367c26752ec3468fdae93f63402c8fa65
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-08-12 16:08:03 +0530

    feat: add variables in js

---
+++ 01-fundamentals/04-variables/script.js
@@ -0,0 +1,66 @@
+
+// JavaScript Variables
+
+
+// let
+
+let name = "Tejas";
+
+console.log(name);
+
+// let variables can be reassigned
+
+let age = 20;
+
+console.log(age);
+
+age = 25;
+
+console.log(age);
+
+
+// const
+
+
+const country = "India";
+
+console.log(country);
+
+
+// const variables cannot be reassigned
+// country = "Malaysia"; // TypeError
+
+
+// var
+
+var city = "Pune";
+
+console.log(city);
+
+// var can be reassigned
+
+city = "Mumbai";
+
+console.log(city);
+
+
+// Multiple Variables
+
+let firstName = "Tejas";
+let lastName = "Kalmegh";
+let currentAge = 25;
+
+console.log(firstName);
+console.log(lastName);
+console.log(currentAge);
+
+
+// Variables with Different Values
+
+let course = "JavaScript";
+let price = 999;
+let isLearning = true;
+
+console.log(course);
+console.log(price);
+console.log(isLearning);
