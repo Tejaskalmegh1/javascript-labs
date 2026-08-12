0975565fbbdfdce4f49722dd5064f5813e812b50
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-08-12 16:03:40 +0530

    feat: add comments in js

---
+++ 01-fundamentals/03-comments/script.js
@@ -0,0 +1,17 @@
+// This is a single-line comment
+
+console.log("JavaScript Comments");
+
+/*
+    This is a
+    multi-line comment.
+*/
+
+console.log("Comments are ignored by JavaScript");
+
+// console.log("This code will not execute");
+
+/*
+console.log("This code");
+console.log("is also commented");
+*/
