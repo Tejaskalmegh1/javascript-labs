6227c480f0b28a7dd37ad5543652fcf8d40f0c64
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-08-12 16:10:39 +0530

    feat: add variable naming in js

---
+++ 01-fundamentals/05-variable-naming/script.js
@@ -0,0 +1,49 @@
+
+// JavaScript Variable Naming
+
+
+// Valid Variable Names
+
+let name = "Tejas";
+let age = 22;
+let firstName = "Tejas";
+let userAge = 22;
+let totalPrice = 1500;
+let isLoggedIn = true;
+let _count = 10;
+let $price = 500;
+
+
+// camelCase Convention
+
+// let firstName = "Tejas";
+let lastName = "Kalmegh";
+let userName = "tejas123";
+let phoneNumber = "1234567890";
+let totalAmount = 5000;
+let isUserLoggedIn = true;
+
+
+// Constants
+
+const PI = 3.14159;
+const MAX_USERS = 100;
+const API_URL = "https://example.com";
+
+// Invalid Variable Names
+
+// let 1name = "Tejas";
+// let user-name = "Tejas";
+// let user name = "Tejas";
+// let let = 10;
+
+
+// Variable Names Are Case-Sensitive
+
+// let name = "Tejas";
+let Name = "Ram";
+let NAME = "Krishna";
+
+console.log(name);
+console.log(Name);
+console.log(NAME);
