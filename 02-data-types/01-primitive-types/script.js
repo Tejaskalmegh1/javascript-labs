96607fd7ddc0a7309b4c0d14cbd6e4473eb1119a
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-08-18 18:01:52 +0530

    feat: add primitive data types in js

---
+++ 02-data-types/01-primitive-types/script.js
@@ -0,0 +1,65 @@
+
+// JavaScript Primitive Data Types
+
+
+// String
+
+let name = "Tejas";
+console.log(name);
+
+
+// Number
+
+let age = 22;
+let price = 999.99;
+
+console.log(age);
+console.log(price);
+
+
+// BigInt
+
+let bigNumber = 123456789012345678901234567890n;
+
+console.log(bigNumber);
+
+
+// Boolean
+
+let isStudent = true;
+let isLoggedIn = false;
+
+console.log(isStudent);
+console.log(isLoggedIn);
+
+// Undefined
+
+let score;
+
+console.log(score);
+
+// Null
+
+let selectedUser = null;
+
+console.log(selectedUser);
+
+
+// Symbol
+
+let id = Symbol("userId");
+
+console.log(id);
+
+
+// List of Primitive Values
+
+console.log("String:", name);
+console.log("Number:", age);
+console.log("BigInt:", bigNumber);
+console.log("Boolean:", isStudent);
+console.log("Undefined:", score);
+console.log("Null:", selectedUser);
+console.log("Symbol:", id);
+
+
